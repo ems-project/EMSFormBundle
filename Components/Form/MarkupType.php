a30b3a87214373092319de17daf08b79dbfc28fa
David Mattei <david.mattei@smals.be>
2019-07-30 14:23:33 +0200

    exclude markup fields from submit data

--- Components/Form/MarkupType.php
+++ Components/Form/MarkupType.php
@@ -2,11 +2,29 @@
 
 namespace EMS\FormBundle\Components\Form;
 
+use EMS\FormBundle\FormConfig\MarkupConfig;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MarkupType extends AbstractType
 {
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+       $view->vars['config'] = $options['config'];
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired('config')
+            ->setAllowedTypes('config', MarkupConfig::class)
+        ;
+
+    }
+
     public function getParent()
     {
         return FormType::class;
