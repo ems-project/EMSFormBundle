31a592c685429d4662e2a87255c19ac01938338a
David Meert <david.meert@smals.be>
2019-10-24 16:45:14 +0200

    feature: allow multi level choice configurations

--- FormConfig/FieldChoicesConfig.php
+++ FormConfig/FieldChoicesConfig.php
@@ -10,6 +10,10 @@ class FieldChoicesConfig
     private $values;
     /** @var array */
     private $labels;
+    /** @var int */
+    private $maxLevel;
+    /** @var array */
+    private $choices = [];
 
     public function __construct(string $id, array $values, array $labels)
     {
@@ -24,8 +28,51 @@ class FieldChoicesConfig
 
     public function list(): array
     {
-        $combine = array_combine($this->labels, $this->values);
+        $combine = array_combine($this->getCurrentLevel($this->labels), $this->getCurrentLevel($this->values));
 
         return is_array($combine) ? $combine : [];
+    }
+
+    public function isMultiLevel(): bool
+    {
+        return $this->getMaxLevel() > 0;
+    }
+    
+    public function getMaxLevel(): int
+    {
+        if ($this->maxLevel !== null) {
+            return $this->maxLevel;
+        }
+        $this->maxLevel = max(0, $this->calculateMaxLevel($this->values));
+        return $this->maxLevel;
+    }
+    
+    private function calculateMaxLevel(array $choices): int
+    {
+        $level = 0;
+        foreach ($choices as $choice) {
+            if (\is_array($choice)) {
+                $level = max(
+                    $level,
+                    1 + $this->calculateMaxLevel($choice[\array_key_first($choice)])
+                );
+            }
+        }
+        return $level;
+    }
+    
+    private function getCurrentLevel(array $elements): array
+    {
+        return \array_filter(
+            \array_map(
+                function ($element) {
+                    if (\is_array($element)) {
+                        return \array_key_first($element);
+                    }
+                    return $element;
+                },
+                $elements
+            )
+        );
     }
 }
