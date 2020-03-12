95be73f0fc952774ddb5a9be75ce7b3ab081d9b7
Jérôme Coppée <j.coppee@gmail.com>
2020-03-12 11:22:20 +0100

    ref(js/validation/belgiumPhone): refactor in class (remove regex)

--- Resources/assets/js/validation/belgiumPhone.js
+++ Resources/assets/js/validation/belgiumPhone.js
@@ -1,46 +1,75 @@
 import {i18n} from "../modules/translations";
 
-export function setBelgiumPhoneValidation(element) {
+export function setBelgiumPhoneValidation(element)
+{
     element.addEventListener('change', function() {
-        if(validateBelgiumPhone(this.value)) {
+        let validator = new BelgiumPhoneNumberValidator(this.value);
+        if(validator.validate()) {
             this.setCustomValidity('');
         } else {
             this.setCustomValidity(i18n.trans('belgium_phone', {string: this.value}));
         }
     });
+}
 
-    function validateBelgiumPhone(value) {
-        const regexLeadingPlus = /^(\+\d\d)\d\d\d\d\d\d\d\d\d?/gm;
-        const regexLeadingZeros = /^(00\d\d)\d\d\d\d\d\d\d\d\d?/gm;
-        let numbers = value.match(/\d+/g);
+export class BelgiumPhoneNumberValidator
+{
+    constructor(value) {
+        this.value = value;
+        this.numbers = this.value.match(/\d+/g);
+    }
 
-        if (numbers === null) {
+    validate() {
+        if (this.numbers === null) {
             return false;
         }
-        let phone = numbers.map(String).join('');
-        let mz;
-        let valid = false;
-        while ((mz = regexLeadingZeros.exec(phone)) !== null) {
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (mz.index === regexLeadingZeros.lastIndex) {
-                regexLeadingZeros.lastIndex++;
-            }
-            valid = valid || (phone.length === 13) || (phone.length === 12);
+
+        this.phone = this.transform();
+
+        const typeNumber = this.getTypeNumber();
+
+        if (this.validateNumberOfDigit(typeNumber)) {
+            return true;
         }
-        if (value.startsWith("+")) {
-            // in this case we add a character
-            phone = ("+").concat(phone);
-            let mp;
-            while ((mp = regexLeadingPlus.exec(phone)) !== null) {
-                // This is necessary to avoid infinite loops with zero-width matches
-                if (mp.index === regexLeadingPlus.lastIndex) {
-                    regexLeadingPlus.lastIndex++;
-                }
-                // Phone with a + ( => a character has been added)
-                valid = valid || (phone.length === 12) || (phone.length === 11);
-            }
-            return valid;
+
+        return false;
+    }
+
+    validateNumberOfDigit(typeNumber) {
+        if (typeNumber === 'zeros') {
+            return (this.phone.length === 13) || (this.phone.length === 12);
         }
-        return valid || (phone.length === 10) || (phone.length === 9);
+
+        if (typeNumber === 'plus') {
+            return (this.phone.length === 12) || (this.phone.length === 11);
+        }
+
+        if (typeNumber === 'local') {
+            return (this.phone.length === 10) || (this.phone.length === 9);
+        }
+
+        return false;
+    }
+
+    getTypeNumber() {
+        if (this.phone.startsWith('+')) {
+            return 'plus';
+        }
+
+        if (this.phone.startsWith('00')) {
+            return 'zeros';
+        }
+
+        return 'local';
+    }
+
+    transform() {
+        let phone = this.numbers.map(String).join('');
+
+        if (this.value.startsWith('+')) {
+            phone = ('+').concat(phone);
+        }
+
+        return phone;
     }
 }
