7c6c48f791d9711ad9128548a8dd308103ebc63a
Jérôme Coppée <j.coppee@gmail.com>
2020-03-12 14:44:42 +0100

    fix(js/validation/belgiumPhone): if we put invalid value and after erase them, the setCustomValidity was still there.

--- Resources/assets/js/validation/belgiumPhone.js
+++ Resources/assets/js/validation/belgiumPhone.js
@@ -3,6 +3,11 @@ import {i18n} from "../modules/translations";
 export function setBelgiumPhoneValidation(element)
 {
     element.addEventListener('change', function() {
+        if(this.value === '') {
+            this.setCustomValidity('');
+            return;
+        }
+
         let validator = new BelgiumPhoneNumberValidator(this.value);
         if(validator.validate()) {
             this.setCustomValidity('');
