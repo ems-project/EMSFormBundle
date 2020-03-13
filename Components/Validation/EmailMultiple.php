6065d8a53fd721b8d4822bc3170401c5d2fe1cab
Jérôme Coppée <j.coppee@gmail.com>
2020-03-13 17:02:29 +0100

    feat(Validation): create EmailMultiple

---
+++ Components/Validation/EmailMultiple.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\FormBundle\Components\Validation;
+
+use EMS\FormBundle\Components\Constraint\IsEmailMultiple;
+use Symfony\Component\Validator\Constraint;
+
+final class EmailMultiple extends AbstractValidation
+{
+    public function getConstraint(): Constraint
+    {
+        return new IsEmailMultiple($this->value);
+    }
+
+    public function getHtml5Attribute(): array
+    {
+        return [];
+    }
+}
