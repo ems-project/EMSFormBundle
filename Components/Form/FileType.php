a3dd63d1521336045e44473ab703519ee9fc1a29
David Mattei <david.mattei@smals.be>
2020-06-29 22:26:51 +0200

    fix: return types

--- Components/Form/FileType.php
+++ Components/Form/FileType.php
@@ -9,18 +9,18 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FileType extends SymfonyFileType
 {
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         parent::configureOptions($resolver);
         $resolver->setRequired('ems_translation_domain');
     }
 
-    public function buildView(FormView $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options): void
     {
         $view->vars['ems_translation_domain'] = $options['ems_translation_domain'];
     }
 
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'ems_file';
     }
