97c536bf3b9f7f351f945a0c2665c5b4dd832632
David mattei <david.mattei@sappico.be>
2020-07-16 13:23:29 +0200

    fix: multi select not working (#109)
    JS formData was not correctly build to work with arrays

--- Resources/assets/js/helpers/form.js
+++ Resources/assets/js/helpers/form.js
@@ -4,13 +4,21 @@ export default class
 {
     static getObjectFromFormData(form)
     {
-        let data = {};
+        let object = {};
         let formData = new FormData(form);
-        formData.forEach(function(value, key){
-            data[key] = value;
+
+        formData.forEach((value, key) => {
+            if(!Object.prototype.hasOwnProperty.call(object, key)){
+                object[key] = value;
+                return;
+            }
+            if(!Array.isArray(object[key])){
+                object[key] = [object[key]];
+            }
+            object[key].push(value);
         });
 
-        return data;
+        return object;
     }
 
     static getFormDataFromObject(obj)
@@ -20,6 +28,13 @@ export default class
             let filename = value.name;
             if (filename !== undefined) {
                 formData.set(key, value, filename);
+                return;
+            }
+
+            if(Array.isArray(value)){
+                for (let i = 0; i < value.length; i++) {
+                    formData.append(key, value[i]);
+                }
                 return;
             }
 
