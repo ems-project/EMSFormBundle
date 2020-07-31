996a58d6cbd63e3e82a18b7caf25f6fbfbff4683
Mathieu De Keyzer <mathieu@dekeyzer.net>
2020-07-31 22:47:34 +0200

    fix: use the session to save a generated confirmation code (due to performance issue)

--- Service/Verification/VerificationService.php
+++ Service/Verification/VerificationService.php
@@ -7,6 +7,7 @@ namespace EMS\FormBundle\Service\Verification;
 use EMS\ClientHelperBundle\Helper\Api\ApiService;
 use EMS\ClientHelperBundle\Helper\Api\Client;
 use EMS\ClientHelperBundle\Helper\Elasticsearch\ClientRequestManager;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 final class VerificationService
 {
@@ -14,23 +15,49 @@ final class VerificationService
     private $apiService;
     /** @var ClientRequestManager */
     private $clientRequestManager;
+    /** @var SessionInterface */
+    private $session;
+    /** @var bool */
+    private $savedInSession;
 
-    public function __construct(ApiService $apiService, ClientRequestManager $clientRequestManager)
+    public function __construct(ApiService $apiService, ClientRequestManager $clientRequestManager, SessionInterface $session, bool $savedInSession = true)
     {
         $this->apiService = $apiService;
         $this->clientRequestManager = $clientRequestManager;
+        $this->session = $session;
+        $this->savedInSession = $savedInSession;
     }
 
     public function create(string $value): ?string
     {
-        return $this->getApiClient()->createFormVerification($value);
+        if (!$this->savedInSession) {
+            return $this->getApiClient()->createFormVerification($value);
+        }
+
+        $verificationCode =  $this->session->get($this->getSessionKey($value), null);
+
+        if ($verificationCode === null) {
+            $verificationCode = \sprintf("%06d", \mt_rand(1, 999999));
+            $this->session->set($this->getSessionKey($value), $verificationCode);
+        }
+
+        return $verificationCode;
     }
 
     public function verify(string $value, string $expectedCode): bool
     {
-        $verificationCode = $this->getApiClient()->getFormVerification($value);
+        if ($this->savedInSession) {
+            $verificationCode =  $this->session->get($this->getSessionKey($value), false);
+        } else {
+            $verificationCode = $this->getApiClient()->getFormVerification($value);
+        }
 
         return $verificationCode === $expectedCode;
+    }
+
+    private function getSessionKey(string $value): string
+    {
+        return \sprintf('EMS_CC_[%s]', $value);
     }
 
     private function getApiClient(): Client
