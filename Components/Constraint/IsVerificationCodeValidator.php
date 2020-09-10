4568b00d2f3d8cae138afacacaa833c78365581a
David Mattei <david.mattei@smals.be>
2020-09-10 15:13:58 +0200

    feat: constraint message confirmation missing

--- Components/Constraint/IsVerificationCodeValidator.php
+++ Components/Constraint/IsVerificationCodeValidator.php
@@ -28,23 +28,20 @@ class IsVerificationCodeValidator extends ConstraintValidator
             return;
         }
 
-        if (!$this->verify($value, $constraint)) {
+        if (null === $verificationValue = $this->getVerificationValue($constraint)) {
+            return;
+        }
+
+        $verificationCode = $this->verificationCodeService->getVerificationCode($verificationValue);
+
+        if (null === $verificationCode) {
+            $this->context->addViolation($constraint->messageMissing);
+            return;
+        }
+
+        if ($verificationCode !== (string) $value) {
             $this->context->addViolation($constraint->message, ['{{code}}' => $value]);
         }
-    }
-
-    /**
-     * @param IsVerificationCode $constraint
-     */
-    private function verify($value, IsVerificationCode $constraint): bool
-    {
-        $verificationValue = $this->getVerificationValue($constraint);
-
-        if (null === $value || null === $verificationValue) {
-            return false;
-        }
-
-        return $this->verificationCodeService->verify($verificationValue, (string) $value);
     }
 
     private function getVerificationValue(IsVerificationCode $constraint): ?string
