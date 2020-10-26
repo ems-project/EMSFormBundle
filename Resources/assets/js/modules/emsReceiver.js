2fff621fcab3e45d10cf065407faa3bc050b1086
Mathieu De Keyzer <me@theus.be>
2020-10-26 11:31:11 +0100

    chore: typo

--- Resources/assets/js/modules/emsReceiver.js
+++ Resources/assets/js/modules/emsReceiver.js
@@ -23,7 +23,7 @@ export class emsReceiver
     onMessage(message)
     {
         if ( !this.domains.includes(message.origin) ) {
-            console.log('Domain not allowed ');
+            console.log('Domain not allowed');
             return;
         }
 
