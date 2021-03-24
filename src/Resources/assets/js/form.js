c7319b9f43b961ebceb3b5bfea2cc4ff8b576c4a
Mathieu De Keyzer <me@theus.be>
2021-03-24 14:31:08 +0100

    feat: hide the load management (the init() can be called as many as we wan)t

--- src/Resources/assets/js/form.js
+++ src/Resources/assets/js/form.js
@@ -6,12 +6,6 @@ document.addEventListener('DOMContentLoaded', defaultLoad);
 export function defaultLoad() {
     if (defaultCheck()) {
         let form = new emsForm();
-        let formActivated = false;
-        form.elementIframe.onload = function() { if (formActivated) return; formActivated = true; form.init(); };
-
-        const iframeDoc = form.elementIframe.contentDocument || form.elementIframe.contentWindow.document;
-        if (  iframeDoc.readyState  == 'complete' && !formActivated ) {
-            form.init();
-        }
+        form.init();
     }
 }
