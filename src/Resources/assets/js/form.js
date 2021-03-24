ef8faccc5649dbaf4124fa901198404b9ea8849a
Mathieu De Keyzer <me@theus.be>
2021-03-24 11:00:59 +0100

    fix: WIP call the init if the iframe is already loaded

--- src/Resources/assets/js/form.js
+++ src/Resources/assets/js/form.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', defaultLoad);
 export function defaultLoad() {
     if (defaultCheck()) {
         let form = new emsForm();
-        form.elementIframe.onload = function() { form.init(); };
+        let formActivated = false;
+        form.elementIframe.onload = function() { if (formActivated) return; formActivated = true; form.init(); };
+
+        const iframeDoc = form.elementIframe.contentDocument || form.elementIframe.contentWindow.document;
+        if (  iframeDoc.readyState  == 'complete' && !formActivated ) {
+            form.init();
+        }
     }
 }
