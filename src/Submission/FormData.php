fcc2c9c277ba90b9feddc657cc2657ba94bdb8d3
Mathieu De Keyzer <me@theus.be>
2021-12-16 13:26:00 +0100

    feat: files as UUID (#265) (#266)
    * feat: convert file fields in uuid strings in order to simplify their usage by the submission handlers
    
    * feat: helper to get choice's label
    
    Co-authored-by: mdk <mathieu.dekeyzer@smals.be>
    
    Co-authored-by: mdk <mathieu.dekeyzer@smals.be>

--- src/Submission/FormData.php
+++ src/Submission/FormData.php
@@ -7,6 +7,7 @@ namespace EMS\FormBundle\Submission;
 use EMS\FormBundle\Components\Field\File;
 use EMS\FormBundle\Components\Field\MultipleFile;
 use EMS\FormBundle\FormConfig\FormConfig;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -15,6 +16,8 @@ final class FormData
     private FormConfig $formConfig;
     /** @var array<string, mixed> */
     private array $raw;
+    /** @var array<string, UploadedFile> */
+    private array $files = [];
 
     /**
      * @param FormInterface<FormInterface> $form
@@ -55,5 +58,31 @@ final class FormData
         }
 
         return $files;
+    }
+
+    public function filesAsUUid(): void
+    {
+        foreach ($this->raw  as $key => &$data) {
+            if (!$data instanceof UploadedFile) {
+                continue;
+            }
+            $uuid = Uuid::uuid1()->toString();
+            $this->files[$uuid] = $this->raw[$key];
+            $this->raw[$key] = $uuid;
+        }
+    }
+
+    public function isFileUuid(string $uuid): bool
+    {
+        return isset($this->files[$uuid]);
+    }
+
+    public function getFileFromUuid(string $uuid): UploadedFile
+    {
+        if (!isset($this->files[$uuid])) {
+            throw new \RuntimeException(\sprintf('File with uuid %s not found', $uuid));
+        }
+
+        return $this->files[$uuid];
     }
 }
