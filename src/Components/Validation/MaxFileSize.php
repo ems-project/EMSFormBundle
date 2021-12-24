bce32537274632d98a9c93a108d9e07586b53934
Jérôme Coppée <j.coppee@gmail.com>
2021-12-24 12:37:07 +0100

    feat(validation): add data attribute to MaxFileSize

--- src/Components/Validation/MaxFileSize.php
+++ src/Components/Validation/MaxFileSize.php
@@ -7,6 +7,15 @@ use Symfony\Component\Validator\Constraints\File;
 
 class MaxFileSize extends AbstractValidation
 {
+    public function getHtml5Attribute(): array
+    {
+        $constraint = $this->getConstraint();
+
+        return [
+            'data-maxfilesize' => $constraint->maxSize
+        ];
+    }
+
     public function getConstraint(): Constraint
     {
         return new File(['maxSize' => $this->value]);
